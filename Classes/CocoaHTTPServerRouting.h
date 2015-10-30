076648990687a9091a971b4b69760c009ccb33c5
Christian Beer <christian.beer@chbeer.de>
2015-10-30 11:53:59 +0100

    Added shared OS X Framework

--- Classes/CocoaHTTPServerRouting.h
+++ Classes/CocoaHTTPServerRouting.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Christian Beer'. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CocoaHTTPServer-Routing.
 FOUNDATION_EXPORT double CocoaHTTPServer_RoutingVersionNumber;
